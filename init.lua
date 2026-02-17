745875cdec1b72be90db644ee3070244c698dc53
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2026-02-17 14:23:10 +0530

    Changed theme

--- init.lua
+++ init.lua
@@ -114,7 +114,7 @@ require('lazy').setup({
     name = 'catppuccin',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme('catppuccin-latte')
+      vim.cmd.colorscheme('catppuccin-mocha')
     end,
   },
 })
