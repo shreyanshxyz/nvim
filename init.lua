3c881057b2f16af694b369e59b65130c31018626
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2026-02-17 19:02:55 +0530

    Add neodiff for git diff and line break & wordwrap

--- init.lua
+++ init.lua
@@ -24,7 +24,8 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-vim.opt.wrap = false
+vim.opt.wrap = true
+vim.opt.linebreak = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
@@ -117,6 +118,78 @@ require('lazy').setup({
       vim.cmd.colorscheme('catppuccin-mocha')
     end,
   },
+  {
+    'esmuellert/codediff.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    cmd = 'CodeDiff',
+    opts = {
+      highlights = {
+        line_insert = 'DiffAdd',
+        line_delete = 'DiffDelete',
+        char_brightness = 1.4,
+      },
+      diff = {
+        disable_inlay_hints = true,
+        max_computation_time_ms = 5000,
+        original_position = 'left',
+        cycle_next_hunk = true,
+        cycle_next_file = true,
+      },
+      explorer = {
+        position = 'left',
+        width = 40,
+        initial_focus = 'explorer',
+        view_mode = 'tree',
+        focus_on_select = true,
+      },
+      history = {
+        position = 'bottom',
+        height = 15,
+        initial_focus = 'history',
+      },
+      keymaps = {
+        view = {
+          quit = 'q',
+          toggle_explorer = '<leader>b',
+          next_hunk = ']c',
+          prev_hunk = '[c',
+          next_file = ']f',
+          prev_file = '[f',
+          diff_get = 'do',
+          diff_put = 'dp',
+          open_in_prev_tab = 'gf',
+          toggle_stage = '-',
+          stage_hunk = '<leader>hs',
+          unstage_hunk = '<leader>hu',
+          discard_hunk = '<leader>hr',
+          show_help = 'g?',
+        },
+        explorer = {
+          select = '<CR>',
+          hover = 'K',
+          refresh = 'R',
+          toggle_view_mode = 'i',
+          stage_all = 'S',
+          unstage_all = 'U',
+          restore = 'X',
+        },
+        history = {
+          select = '<CR>',
+          toggle_view_mode = 'i',
+        },
+        conflict = {
+          accept_incoming = '<leader>ct',
+          accept_current = '<leader>co',
+          accept_both = '<leader>cb',
+          discard = '<leader>cx',
+          next_conflict = ']x',
+          prev_conflict = '[x',
+          diffget_incoming = '2do',
+          diffget_current = '3do',
+        },
+      },
+    },
+  },
 })
 
 vim.keymap.set('n', '<leader>e', ':Neotree toggle position=left<CR>', { noremap = true, silent = true })
@@ -125,6 +198,19 @@ vim.keymap.set({'n', 'v'}, '<C-c>', '"+y', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-x>', '"+dd', { noremap = true, silent = true })
 vim.keymap.set('v', '<C-x>', '"+x', { noremap = true, silent = true })
 vim.keymap.set({'n', 'i'}, '<C-s>', '<cmd>w<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>dd', '<cmd>CodeDiff<CR>', { noremap = true, silent = true, desc = 'Open git diff explorer' })
+vim.keymap.set('n', '<leader>df', '<cmd>CodeDiff file HEAD<CR>', { noremap = true, silent = true, desc = 'Diff current file vs HEAD' })
+vim.keymap.set('n', '<leader>dh', '<cmd>CodeDiff history<CR>', { noremap = true, silent = true, desc = 'Open file history' })
+vim.keymap.set('n', '<leader>dm', '<cmd>CodeDiff merge<CR>', { noremap = true, silent = true, desc = 'Open merge conflict view' })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'codediff',
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+  end,
+})
 
 require('lsp')
 vim.api.nvim_set_keymap('n', '<leader>pf', '<cmd>Telescope find_files<cr>', { noremap = true, silent = true })
