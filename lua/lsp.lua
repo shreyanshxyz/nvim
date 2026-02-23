9a25e06202664c9b445fed2c3106970352b8053c
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2026-02-23 11:00:20 +0530

    feat(lsp): add tab completion keymaps

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -43,6 +43,21 @@ cmp.setup({
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
   }),
   sources = {
     {name = 'nvim_lsp'},
